f509cbacc30488d71ad1049cf2e4734564b82abb
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-01-17 22:21:46 +0100

    Add maps

--- app/src/main/java/com/keepcoding/dragonballavanzado/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/keepcoding/dragonballavanzado/ui/details/DetailsFragment.kt
@@ -10,15 +10,24 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.keepcoding.dragonballavanzado.R
 import com.keepcoding.dragonballavanzado.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class DetailsFragment : Fragment() {
+class DetailsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentDetailsBinding
+    private lateinit var mMap: GoogleMap
+
 
     private val args: DetailsFragmentArgs by navArgs()
     private val viewModel: DetailsViewModel by viewModels()
@@ -35,6 +44,8 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getHeroDetail(args.heroID)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         setListeners()
         setObservers()
@@ -50,6 +61,19 @@ class DetailsFragment : Fragment() {
         }
 
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
 
     private fun setObservers() {
 
